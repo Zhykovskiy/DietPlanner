83cf0506bc843425b85fcbfe0e93102e0033e526
Zhykovskiy <68905616+Zhykovskiy@users.noreply.github.com>
2023-04-09 13:40:37 +0300

    Meal generator Frontend (#11)
    * MealGenerator first alpha
    
    * Implemented meal generator
    
    * Refactor
    
    * Frontend for meal generator
    
    ---------
    
    Co-authored-by: Ponomarov Oleksii <testdrive819@gmail.com>

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { MealService } from 'src/app/shared/meal.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  calories: number = 0;
+  mealSubject = new Subject<any>();
+  mealPlan: any;
+  showMealPlan = false;
+
+  constructor(private router:Router, private _mealService: MealService) { }
 
   ngOnInit(): void {
   }
@@ -16,6 +23,24 @@ export class HomeComponent implements OnInit {
   onLogout(){
     localStorage.removeItem('token');
     this.router.navigate(['user/login']);
+  }
+
+  generateMealPlan() {
+    this._mealService.generateMealPlan(this.calories).subscribe(
+        data => {this.mealSubject.next(data); },
+        error => { console.log(error)}
+      );
+
+    this.mealSubject.subscribe(
+      data => {
+        this.mealPlan = data;
+        this.showMealPlan = true;
+        console.log(this.mealPlan);
+      },
+      error => {
+        console.log(error);
+      }
+    )
   }
 
 }
