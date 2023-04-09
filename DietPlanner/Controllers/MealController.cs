baf6989fb310d6568c777813bc01783342da11a5
Zhukovskiy Dmytro <dimazhykovskiy@gmail.com>
2023-04-09 03:13:47 +0300

    Add meal to plan implemented

--- DietPlanner/Controllers/MealController.cs
+++ DietPlanner/Controllers/MealController.cs
@@ -1,6 +1,10 @@
-﻿using DietPlanner.Models.Meal;
+﻿using DietPlanner.DAL.Entities;
+using DietPlanner.Models.Meal;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace DietPlanner.Controllers
 {
@@ -8,8 +12,16 @@ namespace DietPlanner.Controllers
     [ApiController]
     public class MealController : ControllerBase
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public MealController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         [Route("GenerateMealPlan")]
+        [Authorize]
         public async Task<IActionResult> GenerateMealPlan(double targetCalories)
         {
             try
@@ -36,6 +48,38 @@ namespace DietPlanner.Controllers
             {
                 throw ex;
             }
+        }
+
+        [HttpPost]
+        [Route("AddMealToPlan")]
+        [Authorize]
+        public async Task<IActionResult> AddMealToPlan(List<AddMealToPlanViewModel> model)
+        {
+            var requset = new List<AddToMealPlanRequest>();
+            foreach (var item in model)
+            {
+                requset.Add(new AddToMealPlanRequest
+                {
+                    Date = (int)item.Date.Subtract(new DateTime(1970, 1, 1)).TotalSeconds,
+                    Slot = item.Slot,
+                    Position = item.Position,
+                    Type = item.Type,
+                    Value = item.Value
+                });
+            }
+
+            var json = JsonConvert.SerializeObject(requset);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            var apiKey = "031d6e7cded746119c46900569a5fb0d";
+
+            var url = $@"https://api.spoonacular.com/mealplanner/{currentUser.SpoonacularUserName}/items?hash={currentUser.Hash}&apiKey={apiKey}";
+            using var client = new HttpClient();
+
+            var response = await client.PostAsync(url, data);
+
+            return Ok(response);
         }
     }
 }
